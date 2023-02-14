a0acae513e3a4ffaf0d71e8135c6e2cd72152d28
Ajithkumar k <101916589+ajithkumarkaliyappan@users.noreply.github.com>
2023-02-14 15:56:21 +0530

    Add files via upload

---
+++ brand_name_generator.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 13 17:34:11 2023
+
+@author: ajkaliya
+"""
+
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
